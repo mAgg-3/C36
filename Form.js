b640f1b9b6e456b086427a31ae824aa73403ca33
mAgg-3 <68525402+mAgg-3@users.noreply.github.com>
2020-08-30 16:58:59 -0700

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,33 @@
+class Form {
+  constructor() {
+    this.input=createInput("name");
+    this.button=createButton("play");
+    this.greeting=creatElement("h2");
+  }
+
+  display(){
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    title.position(130, 0);
+    
+    var input = createInput("Name");
+    var button = createButton('Play');
+    this.input.position(130,160);
+    this.button.position(250,200);
+    
+      this.button.mousePressed(function(){
+      this.input.hide();
+      this.button.hide();
+
+      var name = input.value();
+      
+      playerCount+=1;
+      player.update(name)
+      player.updateCount(playerCount);
+      var greeting = createElement('h3');
+      greeting.html("Hello " + name )
+      greeting.position(130, 160)
+    });
+
+  }
+}
